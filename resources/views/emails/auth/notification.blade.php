b6249b53a29f53ad19a8b723f4ae72307a92f45e
Martien Dermawan Tanama <martiendt@gmail.com>
2025-01-06 06:37:44 +0700

    fix email

--- resources/views/emails/auth/notification.blade.php
+++ resources/views/emails/auth/notification.blade.php
@@ -5,17 +5,17 @@
     <br>
     <div class="body-text">
         Hi {{ $username }},
-        <br>
+        <br><br>
         We've detected a login to your account from a device we don't recognize.
         <br>
         If this was you, no action is needed.
         <br>
         If you didn't authorize this login, please secure your account immediately by following these steps: 
-        <br>
+        <br><br>
         1. Reset your password now. <a href="{{ $url }}">{{ $url }}</a>.
-        <br>
+        <br><br>
         Your account security is our top priority. If you need assistance, feel free to contact our support team.
-        <br>
+        <br><br>
         Stay secure,
         <br>
         Pointhub.
